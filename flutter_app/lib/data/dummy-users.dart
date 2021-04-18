22a07b13ffa1ae07655284f59ec03eaa8ca73485
Luís Eduardo Marinho Fernandes <duduyugi@gmail.com>
2021-04-18 12:15:04 -0300

    adicionando user e dummy-users

---
+++ flutter_app/lib/data/dummy-users.dart
@@ -0,0 +1,20 @@
+const DUMMY_USERS = {
+  '1': const User(
+    id: '1',
+    name: 'Maria',
+    email: 'maria@alunos.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2016/04/26/07/20/woman-1353803_960_720.png',
+  ),
+  '2': const User(
+    id: '2',
+    name: 'João',
+    email: 'Joao@nargas.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2013/07/13/10/07/man-156584_960_720.png',
+  ),
+  '3': const User(
+    id: '3',
+    name: 'Julio',
+    email: 'Julio@zipmail.com.br',
+    avatarUrl: 'https://cdn.pixabay.com/photo/2017/01/31/21/23/avatar-2027366_960_720.png',
+  ),
+}
